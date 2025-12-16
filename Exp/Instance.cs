b27fe5a8eea18b53fabff23d929688cdd40271f6
ArcadeMakerSources <160407755+ArcadeMakerSources@users.noreply.github.com>
2025-12-16 10:30:30 +0200

    Create Instance.cs

---
+++ Exp/Instance.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Exp.Spans;
+
+namespace Exp;
+
+public class Instance : IVarSystem
+{
+    internal readonly ClassDefSpan def;
+    public List<Variable> Vars { get; }
+    internal bool IsArray { get; }
+    internal object[] ArrayValues { get; }
+
+    internal Instance(ClassDefSpan def, object[] arrVals = null)
+    {
+        this.def = def;
+        this.Vars = [];
+        this.IsArray = arrVals != null;
+        this.ArrayValues = arrVals;
+
+        foreach (var prop in def.Props)
+        {
+            //object val = prop.InitValueReadText == null ? null : comp.Run<object>(prop.InitValueReadText);
+            Vars.Add(new Variable(prop.Name, null, def, prop.Private, prop.Const));
+        }
+    }
+
+    public override string ToString()
+    {
+        return def == ClassDefSpan.ExpStringDef ? Compiler.ExpStringToString(this) : def.Name;
+    }
+}
